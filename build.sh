28d7f62ed4b35060d63da028e2e982cfd68b9b4d
Hou_jialin <houjialin3@huawei.com>
2026-01-07 17:20:40 +0800

    修改--run_example参数解析逻辑及支持指定950用例执行
    Co-authored-by: Hou_jialin<houjialin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !163 merge br_fix_empopt into master
    
    修改--run_example参数解析逻辑及支持指定950用例执行
    
    Created-by: Hou_jialin
    Commit-by: Hou_jialin
    Merged-by: cann-robot
    Description: ## 描述
    
    - 修改build.sh中run_example参数解析
    - 支持指定--soc=ascend950时，增加执行arch35路径下的用例执行
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    功能测试：
    ![1](https://raw.gitcode.com/user-images/assets/7657293/e4b05db6-1cea-4e1a-995f-a5d95a646e3f/Snipaste_2026-01-05_16-52-22.jpg 'Snipaste_2026-01-05_16-52-22.jpg')
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-cv!163

--- build.sh
+++ build.sh
@@ -444,6 +444,9 @@ check_help_combinations() {
 }
 
 check_param() {
+    if [[ "$ENABLE_RUN_EXAMPLE" == "TRUE" ]]; then
+      ENABLE_CUSTOM=FALSE
+    fi
   # --ops不能与--ophost，--opapi，--opgraph同时存在，如果带U则可以
   if [[ -n "$COMPILED_OPS" && "$ENABLE_TEST" == "FALSE" ]] && [[ "$OP_HOST" == "TRUE" || "$OP_API" == "TRUE" || "$OP_GRAPH" == "TRUE" ]]; then
     echo "[ERROR] --ops cannot be used with --ophost, --opapi, or --opgraph"
@@ -600,6 +603,28 @@ print_error() {
   echo -e "${COLOR_RED}[ERROR] ${msg}${COLOR_RESET}"
   echo $dotted_line
   echo
+}
+
+checkopts_run_example() {
+  ENABLE_RUN_EXAMPLE=TRUE
+  EXAMPLE_NAME="${!OPTIND}"
+  ((OPTIND++))
+  if [[ $OPTIND -le $# ]] && [[ "${!OPTIND}" != --* ]]; then
+    EXAMPLE_MODE="${!OPTIND}"
+    ((OPTIND++))
+  fi
+
+  if [[ $OPTIND -le $# ]] && [[ "${!OPTIND}" != --* ]]; then
+    PKG_MODE="${!OPTIND}"
+    ((OPTIND++))
+    if [[ $OPTIND -le $# ]] && [[ "${!OPTIND}" == --vendor_name* ]]; then
+      VENDOR="${!OPTIND}"
+      VENDOR="${VENDOR#*=}"
+      ((OPTIND++))
+    else
+      VENDOR="custom"
+    fi
+  fi
 }
 
 checkopts() {
@@ -798,7 +823,9 @@ checkopts() {
           ENABLE_UT_EXEC=FALSE
           BUILD_TYPE="Debug"
           ;;
-        run_example) ENABLE_RUN_EXAMPLE=TRUE ;;
+        run_example)
+          checkopts_run_example "$@"
+          ;;
         experimental)
           ENABLE_EXPERIMENTAL=TRUE
           ENABLE_CUSTOM=TRUE
@@ -847,19 +874,6 @@ checkopts() {
         ;;
     esac
   done
-
-  if [[ "$1" == "--run_example" ]]; then
-    EXAMPLE_NAME="$2"
-    EXAMPLE_MODE="$3"
-    if [[ -n "$4" ]]; then
-      PKG_MODE="$4"
-      VENDOR="custom"
-      if [[ -n "$5" ]]; then
-        VENDOR="$5"
-        VENDOR="${VENDOR#*=}"
-      fi
-    fi
-  fi
 
   check_param
   set_create_libs
@@ -1202,7 +1216,14 @@ build_example() {
 
   cd "${BUILD_PATH}"
   if [[ "${EXAMPLE_MODE}" == "eager" ]]; then
-    file=$(find ../ -path "*/${EXAMPLE_NAME}/examples/*" -name test_aclnn_*.cpp)
+    if [[ "$ENABLE_EXPERIMENTAL" == "TRUE" ]]; then
+      file=$(find ../experimental -path "*/${EXAMPLE_NAME}/examples/*" -name test_aclnn_*.cpp)
+    else
+      file=$(find ../ -path "*/${EXAMPLE_NAME}/examples/*" -name test_aclnn_*.cpp -not -path "*/experimental/*")
+      if [[ "$COMPUTE_UNIT" == "ascend910_95" ]]; then
+        file+=($(find ../ -path "*/${EXAMPLE_NAME}/examples/arch35/*" -name test_aclnn_*.cpp))
+      fi
+    fi
     if [ -z "$file" ]; then
       echo "ERROR: ${EXAMPLE_NAME} do not have eager examples"
       exit 1
@@ -1240,7 +1261,14 @@ build_example() {
       fi
     done
   elif [[ "${EXAMPLE_MODE}" == "graph" ]]; then
-    file=$(find ../ -path "*/${EXAMPLE_NAME}/examples/*" -name test_geir_*.cpp)
+    if [[ "$ENABLE_EXPERIMENTAL" == "TRUE" ]]; then
+      file=$(find ../experimental -path "*/${EXAMPLE_NAME}/examples/*" -name test_geir_*.cpp)
+    else
+      file=$(find ../ -path "*/${EXAMPLE_NAME}/examples/*" -name test_geir_*.cpp -not -path "*/experimental/*")
+      if [[ "$COMPUTE_UNIT" == "ascend910_95" ]]; then
+        file+=($(find ../ -path "*/${EXAMPLE_NAME}/examples/arch35/*" -name test_geir_*.cpp))
+      fi
+    fi
     if [ -z "$file" ]; then
       echo "ERROR: ${EXAMPLE_NAME} do not have graph examples"
       exit 1
