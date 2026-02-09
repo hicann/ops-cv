9f98f6b984adba0b0a1f262349e0267afa67845e
liu-wei <lovline.liuwei@huawei.com>
2026-02-09 11:46:11 +0800

    优化run包中参数输入错误时的提示信息
    Co-authored-by: liu-wei<lovline.liuwei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !341 merge master into master
    
    优化run包中参数输入错误时的提示信息
    
    Created-by: liu-wei
    Commit-by: liu-wei
    Merged-by: cann-robot
    Description: ## 描述
    优化run包中参数输入错误时的提示信息
    
    ## 关联的Issue
    无需关联issue。
    
    ## 测试
    功能点测试：
    ```
    $ ./cann-910b-ops-cv_9.0.0_linux-x86_64.run 
    Verifying archive integrity...  100%   SHA256 checksums are OK. All good.
    Uncompressing CANN_OPS-CV_RUN_PACKAGE  100%  
    [OpsCv] [2026-02-09 10:37:43] [INFO]: Start Time: 2026-02-09 10:37:43
    [OpsCv] [ERROR]: ERR_NO:0x0002;ERR_DES: only support one type: full/upgrade/uninstall, operation execute failed! Please use [--help] to see the usage.
    [OpsCv] [2026-02-09 10:37:43] [INFO]: End Time: 2026-02-09 10:37:43
    
    $ ./cann-910b-ops-cv_9.0.0_linux-x86_64.run --full --full
    Verifying archive integrity...  100%   SHA256 checksums are OK. All good.
    Uncompressing CANN_OPS-CV_RUN_PACKAGE  100%  
    [OpsCv] [2026-02-09 10:37:23] [INFO]: Start Time: 2026-02-09 10:37:23
    [OpsCv] [ERROR]: ERR_NO:0x0002;ERR_DES: only support one type: full/upgrade/uninstall, operation execute failed! Please use [--help] to see the usage.
    [OpsCv] [2026-02-09 10:37:23] [INFO]: End Time: 2026-02-09 10:37:23
    ```
    
    常规测试：
    ```
    ====================================
               FINAL SUMMARY
    ====================================
    kirinx90             : FAILED (no .o)
    ascend950            : OK (132 .o)
    ascend910            : SKIPPED (.o)
    ascend910b           : OK (81 .o)
    ascend910B           : OK (81 .o)
    ascend310b           : OK (3 .o)
    ascend310B           : OK (3 .o)
    ascend310p           : OK (25 .o)
    ascend310P           : OK (25 .o)
    ascend610lite        : SKIPPED (.o)
    ascend910_93         : OK (81 .o)
    STATIC_BUILD         : OK (cann-910b-ops-cv_9.0.0_linux-x86_64.run, cann-910b-ops-cv-static_9.0.0_linux-x86_64.tar.gz)
    mc62cm12a            : OK (cann-mc62cm12a-ops-cv_9.0.0_linux-x86_64.run)
    ```
    
    安装卸载测试：
    ```
    [OpsCv] [2026-02-09 10:40:44] [INFO]: Install log file path: (xxx/var/log/ascend_seclog/ascend_install.log)
    [OpsCv] [2026-02-09 10:40:44] [INFO]: Operation log file path: (xxx/var/log/ascend_seclog/operation.log)
    [OpsCv] [2026-02-09 10:40:44] [INFO]: Using requirements: when opp module install finished or before you run the opp module, execute the command [ export ASCEND_OPP_PATH=/xxx/ops/ops-cv/build_ou/cann/opp ] to set the environment path.
    [OpsCv] [2026-02-09 10:40:44] [INFO]: Opp package installed successfully! The new version takes effect immediately.
    
    [OpsCv] [2026-02-09 10:40:54] [INFO]: Delete the installed opp source files in (/xxx/ops/ops-cv/build_ou/cann).
    [OpsCv] [2026-02-09 10:40:56] [INFO]: Delete the install info file (/xxx/ops/ops-cv/build_ou/cann/share/info/ops_cv/ascend_install.info).
    [OpsCv] [2026-02-09 10:40:56] [INFO]: Opp package uninstalled successfully! Uninstallation takes effect immediately.
    [OpsCv] [2026-02-09 10:40:56] [INFO]: Remove precheck info.
    
    ```
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：脚本优化
    
    
    
    See merge request: cann/ops-cv!341

--- scripts/package/ops_cv/scripts/install.sh
+++ scripts/package/ops_cv/scripts/install.sh
@@ -287,7 +287,9 @@ get_opts() {
   done
 
   if [ "$*" = "" ]; then
-    echo "[ERROR]: ERR_NO:${PARAM_INVALID}; ERR_DES:Unrecognized parameters.Try './xxx.run --help for more information.'"
+    echo "[OpsCv] [ERROR]: ERR_NO:${PARAM_INVALID};ERR_DES:\
+ only support one type: full/upgrade/uninstall, operation execute failed!\
+ Please use [--help] to see the usage."
     exitlog
     exit 1
   fi
@@ -348,7 +350,7 @@ get_opts() {
 check_opts() {
   if [ "${CONFLICT_CMD_NUMS}" != 1 ]; then
     echo "[OpsCv] [ERROR]: ERR_NO:${PARAM_INVALID};ERR_DES:\
- only support one type: full/run/devel/upgrade/uninstall/check, operation execute failed!\
+ only support one type: full/upgrade/uninstall, operation execute failed!\
  Please use [--help] to see the usage."
     exitlog
     exit 1
