04f323aa070e4dd724347bf9c54679877e61cf00
ji-songyuan <jisongyuan@h-partners.com>
2026-06-17 11:42:28 +0800

    解决分支st脚本代码冲突
    Co-authored-by: jisongyuan@h-partners.com<jisongyuan@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1007 merge 9.1.0 into 9.1.0
    
    解决分支st脚本代码冲突
    
    Created-by: ji-songyuan
    Commit-by: jisongyuan@h-partners.com
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    解决分支st脚本代码冲突
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    [#542](https://gitcode.com/cann/ops-cv/issues/542) 
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-cv!1007

--- objdetect/roi_align_grad/tests/st/aclnnRoiAlignV2Backward/executor_aclnnRoiAlignV2Backward.py
+++ objdetect/roi_align_grad/tests/st/aclnnRoiAlignV2Backward/executor_aclnnRoiAlignV2Backward.py
@@ -1,32 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-# -----------------------------------------------------------------------------------------------------------
-# Copyright (c) 2025 Huawei Technologies Co., Ltd.
-# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
-=======
 # ----------------------------------------------------------------------------
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
->>>>>>> 8b32882 (新增st)
 # CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
-<<<<<<< HEAD
-# -----------------------------------------------------------------------------------------------------------
-=======
 # ----------------------------------------------------------------------------
->>>>>>> 8b32882 (新增st)
 
 import torch
 import random
 import numpy as np
-<<<<<<< HEAD
-import torchvision
-=======
->>>>>>> 8b32882 (新增st)
 import ctypes
 from atk.configs.dataset_config import InputDataset
 from atk.configs.results_config import TaskResult
@@ -113,11 +99,6 @@ class AsceneRoiAlignV2BackwardApi(BaseApi):
                             y = start_h + ph * size_h + (gh + 0.5) * size_h / grid_h
                             for gw in range(grid_w):
                                 x = start_w + pw * size_w + (gw + 0.5) * size_w / grid_w
-<<<<<<< HEAD
-                                # w1, w2, w3, w4, x_low, x_high, y_low, y_high = bilinear_interpolate_gradient(xH, xW,
-                                #                                                                              y, x)
-=======
->>>>>>> 8b32882 (新增st)
                                 w1, w2, w3, w4, x_low, x_high, y_low, y_high = bilinear_interpolate_gradient(xH, xW,
                                                                                                     y, x, y_diff.dtype.name)
                                 g1, g2, g3, g4 = grad_bin * w1, grad_bin * w2, grad_bin * w3, grad_bin * w4
@@ -128,48 +109,6 @@ class AsceneRoiAlignV2BackwardApi(BaseApi):
                                     res[fm_batch_idx, :, y_high, x_high] += g4
             return torch.from_numpy(res.astype(y_diff.dtype, copy=False))
  
-<<<<<<< HEAD
-        def bilinear_interpolate_gradient_ori(height, width, y, x):
-            # deal with cases that inverse elements are out of feature map boundary
-            if y < -1.0 or y > height or x < -1.0 or x > width:
-                w1 = w2 = w3 = w4 = np.float32(0.)
-                x_low = x_high = y_low = y_high = -1
-                return w1, w2, w3, w4, x_low, x_high, y_low, y_high
- 
-            if y <= 0:
-                y = np.float32(0)
-            if x <= 0:
-                x = np.float32(0)
- 
-            x_low = int(x)
-            y_low = int(y)
- 
-            if y_low >= height - 1:
-                y_high = y_low = height - 1
-                y = np.float32(y_low)
-            else:
-                y_high = y_low + 1
- 
-            if x_low >= width - 1:
-                x_high = x_low = width - 1
-                x = np.float32(x_low)
-            else:
-                x_high = x_low + 1
- 
-            ly = y - np.float32(y_low)
-            lx = x - np.float32(x_low)
-            hy = np.float32(1.) - ly
-            hx = np.float32(1.) - lx
- 
-            w1 = hy * hx
-            w2 = hy * lx
-            w3 = ly * hx
-            w4 = ly * lx
- 
-            return w1, w2, w3, w4, x_low, x_high, y_low, y_high
- 
-=======
->>>>>>> 8b32882 (新增st)
         def bilinear_interpolate_gradient(height, width, y, x, dtype):
             # deal with cases that inverse elements are out of feature map boundary
             if y < -1.0 or y > height or x < -1.0 or x > width:
@@ -209,31 +148,10 @@ class AsceneRoiAlignV2BackwardApi(BaseApi):
  
             return w1, w2, w3, w4, x_low, x_high, y_low, y_high
  
-<<<<<<< HEAD
- 
-        if self.device == "gpu":
-            device = f"cuda:{self.device_id}"
-            output = torch.ops.torchvision._roi_align_backward(input_data.kwargs["gradOutput"].to(device), input_data.kwargs["boxes"].to(device),
-                                                                spatial_scale=input_data.kwargs["spatialScale"], pooled_height=input_data.kwargs["pooledHeight"], 
-                                                                pooled_width=input_data.kwargs["pooledWidth"], batch_size=input_data.kwargs["inputShape"][0], 
-                                                                channels=input_data.kwargs["inputShape"][1], height=input_data.kwargs["inputShape"][2], 
-                                                                width=input_data.kwargs["inputShape"][3], sampling_ratio=input_data.kwargs["samplingRatio"], 
-                                                                aligned=input_data.kwargs["aligned"])
-            return output
-        elif self.device == "npu":
-            device = f"{self.device}:{self.device_id}"
-        else:
-            device = "cpu"
-            gradInput = roi_align_grad_compute(input_data.kwargs["gradOutput"].numpy(), input_data.kwargs["boxes"].numpy(),
-                                               input_data.kwargs["inputShape"], input_data.kwargs["spatialScale"],
-                                               input_data.kwargs["samplingRatio"], input_data.kwargs["aligned"])
-            return gradInput
-=======
         gradInput = roi_align_grad_compute(input_data.kwargs["gradOutput"].numpy(), input_data.kwargs["boxes"].numpy(),
                                             input_data.kwargs["inputShape"], input_data.kwargs["spatialScale"],
                                             input_data.kwargs["samplingRatio"], input_data.kwargs["aligned"])
         return gradInput
->>>>>>> 8b32882 (新增st)
 
 @register("aclnn_function")
 class aclnnfunctionExecutor(AclnnBaseApi):
