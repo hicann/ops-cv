9f84cfd304354b61b903399e3adc857a9c19425d
chenhaijie1423 <chenhaijie2@h-partners.com>
2026-09-17 20:20:34 +0800

    [Fix] 重命名 8 个 example 文件以匹配接口名
    Co-authored-by: chenhaijie<chenhaijie2@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1643 merge resize into master
    
    [Fix] 重命名 8 个 example 文件以匹配接口名
    
    Created-by: chenhaijie1423
    Commit-by: chenhaijie
    Merged-by: cann-robot
    Description: ## 描述
    [Fix] 重命名 8 个 example 文件以匹配接口名
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-cv/issues/967
    
    ## 测试
    example重命名，不修改内容
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-cv!1643

--- image/upsample_nearest_exact2d_grad/examples/test_aclnn_upsample_nearest_exact2d_backward.cpp
+++ image/upsample_nearest_exact2d_grad/examples/test_aclnn_upsample_nearest_exact2d_backward.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <vector>
 #include "acl/acl.h"
-#include "aclnnop/aclnn_grid_sampler2d.h"
+#include "aclnnop/aclnn_upsample_nearest_exact2d_backward.h"
 
 #define CHECK_RET(cond, return_expr) \
     do {                             \
@@ -47,8 +47,8 @@ int Init(int32_t deviceId, aclrtStream* stream)
 }
 
 template <typename T>
-int CreateAclTensor(const std::vector<T>& hostData, const std::vector<int64_t>& shape, void** deviceAddr,
-                    aclDataType dataType, aclTensor** tensor)
+int CreateAclNchTensor(const std::vector<T>& hostData, const std::vector<int64_t>& shape, void** deviceAddr,
+                       aclDataType dataType, aclTensor** tensor)
 {
     auto size = GetShapeSize(shape) * sizeof(T);
     // 调用aclrtMalloc申请device侧内存
@@ -65,7 +65,7 @@ int CreateAclTensor(const std::vector<T>& hostData, const std::vector<int64_t>& 
     }
 
     // 调用aclCreateTensor接口创建aclTensor
-    *tensor = aclCreateTensor(shape.data(), shape.size(), dataType, strides.data(), 0, aclFormat::ACL_FORMAT_ND,
+    *tensor = aclCreateTensor(shape.data(), shape.size(), dataType, strides.data(), 0, aclFormat::ACL_FORMAT_NCHW,
                               shape.data(), shape.size(), *deviceAddr);
     return 0;
 }
@@ -80,83 +80,68 @@ int main()
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("Init acl failed. ERROR: %d\n", ret); return ret);
 
     // 2. 构造输入与输出，需要根据API的接口自定义构造
-    int64_t interpolationMode = 0;
-    int64_t paddingMode = 0;
-    bool alignCorners = false;
-    std::vector<int64_t> inputShape = {1, 1, 5, 8};
-    std::vector<int64_t> gridShape = {1, 3, 3, 2};
-    std::vector<int64_t> outShape = {1, 1, 3, 3};
+    std::vector<int64_t> inputShape = {1, 1, 4, 2};
+    std::vector<int64_t> outShape = {1, 1, 8, 4};
     void* inputDeviceAddr = nullptr;
-    void* gridDeviceAddr = nullptr;
     void* outDeviceAddr = nullptr;
     aclTensor* input = nullptr;
-    aclTensor* grid = nullptr;
     aclTensor* out = nullptr;
-
-    std::vector<float> inputHostData = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                                        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
-    std::vector<float> gridHostData = {-1, -1, 0, -1, 1, -1, -1, 0, 0, 0, 1, 0, -1, 1, 0, 1, 1, 1};
-    std::vector<float> outHostData = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-
+    std::vector<float> inputHostData = {0, 1, 2, 3, 4, 5, 6, 7};
+    std::vector<float> outHostData(32, 0);
+    std::vector<int64_t> outputSize = {4, 2};
+    std::vector<int64_t> inputSize = {1, 1, 8, 4};
+    double scalesH = 0.5;
+    double scalesW = 0.5;
     // 创建input aclTensor
-    ret = CreateAclTensor(inputHostData, inputShape, &inputDeviceAddr, aclDataType::ACL_FLOAT, &input);
+    ret = CreateAclNchTensor(inputHostData, inputShape, &inputDeviceAddr, aclDataType::ACL_FLOAT, &input);
     CHECK_RET(ret == ACL_SUCCESS, return ret);
-    // 创建grid aclTensor
-    ret = CreateAclTensor(gridHostData, gridShape, &gridDeviceAddr, aclDataType::ACL_FLOAT, &grid);
-    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建input aclIntArray
+    auto outputSizeArray = aclCreateIntArray(outputSize.data(), 2);
+    auto inputSizeArray = aclCreateIntArray(inputSize.data(), 4);
     // 创建out aclTensor
-    ret = CreateAclTensor(outHostData, outShape, &outDeviceAddr, aclDataType::ACL_FLOAT, &out);
+    ret = CreateAclNchTensor(outHostData, outShape, &outDeviceAddr, aclDataType::ACL_FLOAT, &out);
     CHECK_RET(ret == ACL_SUCCESS, return ret);
 
     // 3. 调用CANN算子库API，需要修改为具体的Api名称
     uint64_t workspaceSize = 0;
     aclOpExecutor* executor;
-    // 调用aclnnGridSampler2D第一段接口
-    ret = aclnnGridSampler2DGetWorkspaceSize(input, grid, interpolationMode, paddingMode, alignCorners, out,
-                                             &workspaceSize, &executor);
-    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGridSampler2DGetWorkspaceSize failed. ERROR: %d.\n[ERROR msg]%s", ret,
-                                            aclGetRecentErrMsg());
+    // 调用aclnnUpsampleNearestExact2dBackward第一段接口
+    ret = aclnnUpsampleNearestExact2dBackwardGetWorkspaceSize(input, outputSizeArray, inputSizeArray, scalesH, scalesW,
+                                                              out, &workspaceSize, &executor);
+    CHECK_RET(ret == ACL_SUCCESS,
+              LOG_PRINT("aclnnUpsampleNearestExact2dBackwardGetWorkspaceSize failed. ERROR: %d\n", ret);
               return ret);
     // 根据第一段接口计算出的workspaceSize申请device内存
     void* workspaceAddr = nullptr;
     if (workspaceSize > 0) {
         ret = aclrtMalloc(&workspaceAddr, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
-        CHECK_RET(ret == ACL_SUCCESS,
-                  LOG_PRINT("allocate workspace failed. ERROR: %d.\n[ERROR msg]%s", ret, aclGetRecentErrMsg());
-                  return ret);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("allocate workspace failed. ERROR: %d\n", ret); return ret);
     }
-    // 调用aclnnGridSampler2D第二段接口
-    ret = aclnnGridSampler2D(workspaceAddr, workspaceSize, executor, stream);
-    CHECK_RET(ret == ACL_SUCCESS,
-              LOG_PRINT("aclnnGridSampler2D failed. ERROR: %d.\n[ERROR msg]%s", ret, aclGetRecentErrMsg());
+    // 调用aclnnUpsampleNearestExact2dBackward第二段接口
+    ret = aclnnUpsampleNearestExact2dBackward(workspaceAddr, workspaceSize, executor, stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnUpsampleNearestExact2dBackward failed. ERROR: %d\n", ret);
               return ret);
 
     // 4. （固定写法）同步等待任务执行结束
     ret = aclrtSynchronizeStream(stream);
-    CHECK_RET(ret == ACL_SUCCESS,
-              LOG_PRINT("aclrtSynchronizeStream failed. ERROR: %d.\n[ERROR msg]%s", ret, aclGetRecentErrMsg());
-              return ret);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtSynchronizeStream failed. ERROR: %d\n", ret); return ret);
 
     // 5. 获取输出的值，将device侧内存上的结果复制至host侧，需要根据具体API的接口定义修改
     auto size = GetShapeSize(outShape);
     std::vector<float> resultData(size, 0);
     ret = aclrtMemcpy(resultData.data(), resultData.size() * sizeof(resultData[0]), outDeviceAddr,
                       size * sizeof(resultData[0]), ACL_MEMCPY_DEVICE_TO_HOST);
-    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("copy resultData from device to host failed. ERROR: %d.\n[ERROR msg]%s",
-                                            ret, aclGetRecentErrMsg());
-              return ret);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("copy result from device to host failed. ERROR: %d\n", ret); return ret);
     for (int64_t i = 0; i < size; i++) {
-        LOG_PRINT("resultData[%ld] is: %f\n", i, resultData[i]);
+        LOG_PRINT("result[%ld] is: %f\n", i, resultData[i]);
     }
 
-    // 6. 释放aclTensor，需要根据具体API的接口定义修改
+    // 6. 释放aclTensor和aclIntArray，需要根据具体API的接口定义修改
     aclDestroyTensor(input);
-    aclDestroyTensor(grid);
     aclDestroyTensor(out);
 
-    // 7. 释放Device资源，需要根据具体API的接口定义修改
+    // 7. 释放device资源，需要根据具体API的接口定义修改
     aclrtFree(inputDeviceAddr);
-    aclrtFree(gridDeviceAddr);
     aclrtFree(outDeviceAddr);
     if (workspaceSize > 0) {
         aclrtFree(workspaceAddr);
@@ -164,6 +149,5 @@ int main()
     aclrtDestroyStream(stream);
     aclrtResetDevice(deviceId);
     aclFinalize();
-
     return 0;
 }
